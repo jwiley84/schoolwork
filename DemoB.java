0b3a898d58b1f74d2413b66c4fa2ba2f76a0187a
Jessica Wailes <jess.m.wailes@gmail.com>
2017-05-19 12:48:55 -0700

    update in math class

--- DemoB.java
+++ DemoB.java
@@ -3,26 +3,26 @@
    public class DemoB {
    public static void main(String[] args) {
 	double average;   
-   StudentA std1, std2, std3, std4;  //Declare objects of type  Class <Student>
+   StudentB std1, std2, std3, std4;  //Declare objects of type  Class <Student>
  // Build student objects using "default" constructor
-     std1= new StudentB();     //Create new object belonging to Class <Student>
+    std1= new StudentB();     //Create new object belonging to Class <Student>
     std2= new StudentB();   //create second object belonging to Class <Student>
-                       // stores reference to that object in <std2> which is different for <std1 >
-    std3=null;
-    std3= std1;      
-    System.out.println("std3 memory address in hex  "  +  std3);  // outputs the Hex the point of std1
-            //
+    std3= new StudentB();
+    std4=null;    
+    //
     // add data in to object <std1> and <std2>
-    std1.name="Joe";
-    std2.name="Bill";
-    System.out.println("std1 name    "  +   std1.name);   // Joe
-    System.out.println("std2 name    "  +   std2.name);   // Bill               
-    System.out.println("std3 name    "  +   std3.name);   // Joe again
-    // Arrays in Student class
+    std1.name="Jessica_Wailes";
+    std2.name="Matthew";
+    std3.name="Nick";
+    System.out.println("std1 name    "  +   std1.name);  
+    System.out.println("std2 name    "  +   std2.name);        
+    System.out.println("std3 name    "  +   std3.name);   
+    std4 = std1;
+    System.out.println("std4 matches std1: " + std4.name);
+    
+    //set grades
+    
    
-    System.out.println("array in class student std1 "+std1.testArray ); 
-    System.out.println("array in class student std2 "+std2.testArray ); 
-    System.out.println("array in class student std3 "+std3.testArray );
     average= std1.getAverage(); // "call" method in class Student
     System.out.println(std1.name +"  average    "  +  average);    
       }// End Main
